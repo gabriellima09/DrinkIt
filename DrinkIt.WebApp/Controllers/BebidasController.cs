4d7c1cf4d31446c1bf663f3fcd6e49afe7d7210b
Gustavo da Rosa <guga-rosa13@hotmail.com>
2019-08-11 20:36:26 -0300

    Nova partial View com dashboard adicionada.

--- DrinkIt.WebApp/Controllers/BebidasController.cs
+++ DrinkIt.WebApp/Controllers/BebidasController.cs
@@ -82,6 +82,38 @@ namespace DrinkIt.WebApp.Controllers
                     DicaConservacao = "Beba mais água",
                     Status = "ATIVO"
 
+                },
+
+                new Bebida
+                {
+                    Id = 3,
+                    Nome = "Café",
+                    Descricao = "Café Preto",
+                    Marca = "Pilão",
+                    Valor = 1.99M,
+                    Volume = "600ml",
+                    Peso = "740mg",
+                    Sabor = "Café",
+                    Lote = "12421",
+                    DataFabricacao = DateTime.Now,
+                    DataValidade = DateTime.Now,
+                    Fabricante = "Starbucks",
+                    Embalagem = "Caixinha",
+                    CodigoBarras = "662607004",
+                    Alcoolico = false,
+                    Teor = "0%",
+                    Gaseificada = false,
+                    ContemGluten = false,
+                    Ingredientes = new List<Ingrediente>
+                    {
+                        new Ingrediente
+                        {
+                            Descricao = "Cafeína"
+                        }
+                    },
+                    DicaConservacao = "Manter em local seco e arejado.",
+                    Status = "ATIVO"
+
                 }
 
             };
@@ -221,6 +253,112 @@ namespace DrinkIt.WebApp.Controllers
             {
                 return View();
             }
+        }
+
+        public ActionResult PvDashBebidas()
+        {
+            List<Bebida> bebidas = new List<Bebida>
+            {
+
+                new Bebida
+                {
+                    Id = 1,
+                    Nome = "Crystal",
+                    Descricao = "Água Mineral sem Gás",
+                    Marca = "Crystal",
+                    Valor = 1.99M,
+                    Volume = "1.5L",
+                    Peso = "1KG",
+                    Sabor = "---",
+                    Lote = "12321",
+                    DataFabricacao = DateTime.Now,
+                    DataValidade = DateTime.Now,
+                    Fabricante = "Coca-Cola",
+                    Embalagem = "Garrafa",
+                    CodigoBarras = "662607004",
+                    Alcoolico = false,
+                    Teor = "0%",
+                    Gaseificada = false,
+                    ContemGluten = false,
+                    Ingredientes = new List<Ingrediente>
+                    {
+                        new Ingrediente
+                        {
+                            Descricao = "H2O"
+                        }
+                    },
+                    DicaConservacao = "Beba água",
+                    Status = "ATIVO"
+
+                },
+
+                new Bebida
+                {
+                    Id = 2,
+                    Nome = "Soda",
+                    Descricao = "Soda Limonada",
+                    Marca = "Soda",
+                    Valor = 1.99M,
+                    Volume = "350ml",
+                    Peso = "350mg",
+                    Sabor = "Limão",
+                    Lote = "12321",
+                    DataFabricacao = DateTime.Now,
+                    DataValidade = DateTime.Now,
+                    Fabricante = "Nem sei",
+                    Embalagem = "Lata",
+                    CodigoBarras = "662607004",
+                    Alcoolico = false,
+                    Teor = "0%",
+                    Gaseificada = true,
+                    ContemGluten = false,
+                    Ingredientes = new List<Ingrediente>
+                    {
+                        new Ingrediente
+                        {
+                            Descricao = "Soda"
+                        }
+                    },
+                    DicaConservacao = "Beba mais água",
+                    Status = "ATIVO"
+
+                },
+
+                new Bebida
+                {
+                    Id = 3,
+                    Nome = "Café",
+                    Descricao = "Café Preto",
+                    Marca = "Pilão",
+                    Valor = 1.99M,
+                    Volume = "600ml",
+                    Peso = "740mg",
+                    Sabor = "Café",
+                    Lote = "12421",
+                    DataFabricacao = DateTime.Now,
+                    DataValidade = DateTime.Now,
+                    Fabricante = "Starbucks",
+                    Embalagem = "Caixinha",
+                    CodigoBarras = "662607004",
+                    Alcoolico = false,
+                    Teor = "0%",
+                    Gaseificada = false,
+                    ContemGluten = false,
+                    Ingredientes = new List<Ingrediente>
+                    {
+                        new Ingrediente
+                        {
+                            Descricao = "Cafeína"
+                        }
+                    },
+                    DicaConservacao = "Manter em local seco e arejado.",
+                    Status = "ATIVO"
+
+                }
+
+            };
+
+            return PartialView(bebidas);
         }
     }
 }
