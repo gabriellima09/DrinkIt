e55b873e779044675121de650f1db1ca2e9b29be
Gustavo da Rosa <guga-rosa13@hotmail.com>
2019-08-20 22:41:05 -0300

    IStrategy adicionada, BebidasController agora com fachada.

---
+++ DrinkIt.WebApp/Strategy/IStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkIt.WebApp.Strategy
+{
+    public interface IStrategy<T>
+    {
+        bool Processar(T entidade);
+    }
+}
