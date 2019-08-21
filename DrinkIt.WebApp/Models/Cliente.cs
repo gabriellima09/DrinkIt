d2c6854fbca33e419a7aefef352263cd035b8512
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2019-08-21 20:06:49 -0300

    Trocado dominio de cliente e endereço

--- DrinkIt.WebApp/Models/Cliente.cs
+++ DrinkIt.WebApp/Models/Cliente.cs
@@ -9,11 +9,6 @@ namespace DrinkIt.WebApp.Models
         public DateTime DataNascimento { get; set; }
         public string Cpf { get; set; }
         public string Telefone { get; set; }
-        public string Endereco { get; set; }
-        public string Bairro { get; set; }
-        public string CEP { get; set; }
-        public string Complemento { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
+        public Endereco Endereco { get; set; }
     }
 }
---
+++ DrinkIt.WebApp/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace DrinkIt.WebApp.Models
+{
+    public class Endereco
+    {
+        public string Descricao { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public bool Cobranca { get; set; }
+        public bool Entrega { get; set; }
+    }
+}
