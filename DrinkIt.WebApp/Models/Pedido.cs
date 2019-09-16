ab796bdc9d8bcf1738da9db1f73f304f19597b2b
Gabriel Lima Gomes <gabriel.gomes@infraclearsalecom.onmicrosoft.com>
2019-09-16 17:16:50 -0300

    Correção de modelo de pedido

--- DrinkIt.WebApp/Models/Pedido.cs
+++ DrinkIt.WebApp/Models/Pedido.cs
@@ -8,6 +8,11 @@ namespace DrinkIt.WebApp.Models
         public List<Bebida> Bebidas { get; set; }
         public Cliente Cliente { get; set; }
         public DateTime Data { get; set; }
+        public Endereco Endereco { get; set; }
+        public CartaoCredito Cartao1 { get; set; }
+        public CartaoCredito Cartao2 { get; set; }
+        public Cupom CupomDesconto { get; set; }
+        public Cupom CupomTroca { get; set; }
         public string Status { get; set; }
     }
 }
