02dbd6d79c159330d27fc90ae5951a0129eb8d84
Gabriel Lima Gomes <gabriel.gomes@infraclearsalecom.onmicrosoft.com>
2019-10-01 11:10:59 -0300

    add classe de criptografia

---
+++ DrinkIt.WebApp/Cryptography/Criptografia.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DrinkIt.WebApp.Cryptography
+{
+    public class Criptografia
+    {
+        public string RetornarMD5(string Senha)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                return RetonarHash(md5Hash, Senha);
+            }
+        }
+
+        public bool CompararMD5(string senhabanco, string Senha_MD5)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                var senha = RetornarMD5(senhabanco);
+                if (VerificarHash(Senha_MD5, senha))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private string RetonarHash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        private bool VerificarHash(string input, string hash)
+        {
+            StringComparer compara = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == compara.Compare(input, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
