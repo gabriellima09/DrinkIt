32bc633a3030918932a2ee938df4623b5eb008a0
Gustavo da Rosa <guga-rosa13@hotmail.com>
2019-11-04 16:52:43 -0300

    Selenium + Mais Bebidas

---
+++ DrinkIt.WebApp/Selenium/FluxoGraficos/TesteFluxoGrafico.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace DrinkIt.WebApp.Selenium.FluxoGraficos
+{
+    public class TesteFluxoGrafico
+    {
+        [Fact]
+        public void FluxoGrafico()
+        {
+            TelasFluxoGrafico tela = new TelasFluxoGrafico(Browser.Chrome);
+
+            tela.LoginAdmin();
+            tela.Esperar(7);
+            tela.InserirDatas();
+            tela.Esperar(40);    
+            tela.Fechar();
+        }
+    }
+}
