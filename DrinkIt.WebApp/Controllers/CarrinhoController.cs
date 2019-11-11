7a6374ef1c019ada62dec8afce290f4dcc8d0702
Gustavo da Rosa <guga-rosa13@hotmail.com>
2019-11-11 03:46:17 -0300

    Carrinho - Controle de Quantidade por Estoque

--- DrinkIt.WebApp/Controllers/CarrinhoController.cs
+++ DrinkIt.WebApp/Controllers/CarrinhoController.cs
@@ -1,5 +1,7 @@
-﻿using DrinkIt.WebApp.Models;
+﻿using DrinkIt.WebApp.Dao;
+using DrinkIt.WebApp.Models;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace DrinkIt.WebApp.Controllers
@@ -9,14 +11,22 @@ namespace DrinkIt.WebApp.Controllers
         // GET: Carrinho
         public ActionResult Index()
         {
+            Dictionary<int, int> EstoqueBebidas = new Dictionary<int, int>();
+            EstoqueDao estoquedao = new EstoqueDao();
             Usuario usuario = (Usuario)Session["Usuario"] ?? new Usuario();
-
+            foreach(var item in usuario.Carrinho.Bebidas)
+            {
+                EstoqueBebidas[item.Id] = estoquedao.ConsultarEstoquePorId(item.Id);
+            }
+            ViewBag.Estoque = EstoqueBebidas;
             return View(usuario.Carrinho);
         }
 
         [HttpPost]
         public ActionResult AdicionarBebidaSessao(string bebida)
         {
+            Dictionary<int, int> EstoqueBebidas = new Dictionary<int, int>();
+            EstoqueDao estoquedao = new EstoqueDao();
             Bebida novaBebida = JsonConvert.DeserializeObject<Bebida>(bebida);
 
             novaBebida.Quantidade = 1;
@@ -26,7 +36,13 @@ namespace DrinkIt.WebApp.Controllers
             if(!usuario.Carrinho.Bebidas.Exists(x => x.Id == novaBebida.Id))
             {
                 usuario.Carrinho.Bebidas.Add(novaBebida);
-            }            
+            }
+
+            foreach (var item in usuario.Carrinho.Bebidas)
+            {
+                EstoqueBebidas[item.Id] = estoquedao.ConsultarEstoquePorId(item.Id);
+            }
+            ViewBag.Estoque = EstoqueBebidas;
 
             Session["Usuario"] = usuario;
 
@@ -36,9 +52,17 @@ namespace DrinkIt.WebApp.Controllers
         [HttpPost]
         public ActionResult RemoverBebidaSessao(int idBebida)
         {
+            Dictionary<int, int> EstoqueBebidas = new Dictionary<int, int>();
+            EstoqueDao estoquedao = new EstoqueDao();
             Usuario usuario = (Usuario)Session["Usuario"] ?? new Usuario();
 
             usuario.Carrinho.Bebidas.RemoveAll(x => x.Id.Equals(idBebida));
+
+            foreach (var item in usuario.Carrinho.Bebidas)
+            {
+                EstoqueBebidas[item.Id] = estoquedao.ConsultarEstoquePorId(item.Id);
+            }
+            ViewBag.Estoque = EstoqueBebidas;
 
             Session["Usuario"] = usuario;
 
--- DrinkIt.WebApp/Dao/EstoqueDao.cs
+++ DrinkIt.WebApp/Dao/EstoqueDao.cs
@@ -120,5 +120,20 @@ namespace DrinkIt.WebApp.Dao
 
             return true;
         }
+
+        public int ConsultarEstoquePorId(int IdBebida)
+        {
+            int qtde = 0;
+            Sql.Append("SELECT QTDE FROM ESTOQUE WHERE IDBEBIDA = " + IdBebida);
+            using (var reader = DbContext.ExecuteReader(Sql.ToString()))
+            {
+                if (reader.Read())
+                {
+                    qtde = reader.GetInt32(0);
+                }
+            }
+
+            return qtde;
+        }
     }
 }
